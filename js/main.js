87795146ca920cc8afa3d1773ffc9736ef420f29
Shoykot-Ahamed <shoykotahamed7736@gmail.com>
2020-07-28 19:13:51 +0600

    clock

--- js/main.js
+++ js/main.js
@@ -91,6 +91,20 @@ $('.owl-carousel').owlCarousel({
     }
 })
 
+/* clock */
+const deg = 6;
+const hr = document.querySelector("#hr");
+const mn = document.querySelector("#mn");
+const sc = document.querySelector("#sc");
+setInterval(()=>{
+	let day = new Date();
+	let hh = day.getHours() * 30;
+	let mm = day.getMinutes() * deg;
+	let ss = day.getSeconds() * deg;
+	hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
+	mn.style.transform = `rotateZ(${mm}deg)`;
+	sc.style.transform = `rotateZ(${ss}deg)`;
+})
 
 /* magnificPopup img view */
 $('.popup-image').magnificPopup({
